e796f47dae88980d8b7694c875625e306a364993
Kandy1116 <venus.kandy@gmail.com>
2026-02-16 14:26:30 -0600

    update work

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,24 @@
-import { useState } from "react";
-import Counter2 from "./Counter2";
-import { useSelector, useDispatch } from "react-redux";
+import { useState } from 'react';
+import Counter2 from './Counter2';
+import { useSelector } from 'react-redux';
 
 export default function Counter() {
 
-    const count = 0
-    const count2 = useSelector(state => {
-        console.log(state)
-    })
 
-  return (
+const count = 0
+
+const count2 = useSelector(state => {
+  console.log(state)
+})
+
+return (
     <div>
       <h1>Count is {count}</h1>
       <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
       >
         Increment
       </button>
       <button
-        onClick={() => {
-          setCount(count - 1);
-        }}
       >
         Decrement
       </button>
